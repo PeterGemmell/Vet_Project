3e166a58324bec92aafc0baddb253a4f7c0e84aa
Peter Gemmell <petergemmell@peters-air.home>
2020-02-01 15:02:14 +0000

    Vet Class created

---
+++ models/vet.rb
@@ -0,0 +1,52 @@
+require_relative('../db/sql_runner')
+
+class Vet
+
+  attr_reader :id
+  attr_accessor :name, :url
+
+  def initialize(options)
+    @id = options['id'].to_i if options ['id']
+    @name = options['name']
+    @url = options['url']
+  end
+
+#CREATE
+def save()
+  sql = "INSERT INTO vets(name, url)
+  VALUES($1,$2) RETURNING id"
+  values = [@name, @url]
+  @id = SqlRunner.run(sql, values)[0]['id'].to_i
+end
+
+#READ
+def self.all()
+  sql = "SELECT * FROM vets"
+  vets = SqlRunner.run(sql)
+  return vets.map { |vet|Vet.new(vet) }
+end
+
+def self.find_by_id(id)
+  sql = "SELECT * FROM vets WHERE id = $1"
+  values = [id]
+  vet = SqlRunner.run(sql, values)[0]
+  return Vet.new(vet)
+end
+
+#UPDATE
+def update()
+  sql = "UPDATE vets SET(name, url) = ($1, $2)
+  WHERE id = $3"
+  values = [@name, @url, @id]
+  SqlRunner.run(sql, values)
+end
+
+#DELETE 
+def delete()
+  sql = "DELETE FROM vets
+  WHERE id = $1"
+  values = [@id]
+  SqlRunner.run(sql, values)
+end
+
+end
