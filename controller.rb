8d18b77b5b985bbb63ae9fe5bb5c6dd4becd49ef
Peter Gemmell <petergemmell@Peters-MacBook-Air.local>
2020-02-02 21:05:33 +0000

    erb updates minus edit and delete buttons

--- controller.rb
+++ controller.rb
@@ -6,6 +6,7 @@ require_relative('./models/vet.rb')
 also_reload('./models/*')
 
 
+
 get '/' do
   erb(:index)
 end
@@ -25,8 +26,33 @@ get '/pets/new' do
   erb(:new)
 end
 
+get '/pets/:id' do
+  pet_id = params[:id]
+  @pet = Pet.find(pet_id)
+  erb(:show)
+end
+
 post '/pets' do
   @pet = Pet.new(params)
   @pet.save()
-  erb(:created) 
+  erb(:created)
+end
+
+get '/pets/:id/edit' do
+  pet_id = params[:id]
+  @pet = Pet.find(pet_id)
+  erb(:edit)
+end
+
+post '/pets/:id/delete' do
+  pet_id = params[:id]
+  @pet = Pet.find(pet_id)
+  @pet.delete
+  erb(:delete)
+end
+
+post '/pets/:id' do
+  pet = Pet.new(params)
+  pet.update
+  redirect to '/pets'
 end
